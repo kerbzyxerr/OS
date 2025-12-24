40bc12e8426b34540638453fb74338aae6c52476
kerbzyxerr <155954316+kerbzyxerr@users.noreply.github.com>
2025-12-24 12:59:45 +0300

    Delete lab1 directory

--- lab1/include/child.h
+++
@@ -1,6 +0,0 @@
-#ifndef CHILD_H
-#define CHILD_H
-
-void run_child_process(const char* filename);
-
-#endif
--- lab1/include/parent.h
+++
@@ -1,6 +0,0 @@
-#ifndef PARENT_H
-#define PARENT_H
-
-void run_parent_process(const char* filename);
-
-#endif
